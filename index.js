4a1623cb655351c9b82228f533ac1b8e72eccefa
Eduardo Henrique <itsduzao@outlook.com>
2025-02-03 15:35:48 -0300

    feat: add likedBy rendering logic to display users who liked a post

--- index.js
+++ index.js
@@ -61,12 +61,13 @@ function createPostElement(data) {
     avatar,
     post,
     caption,
+    likedBy,
     date,
     commentsCount,
   } = data
   const postElement = document.createElement('article')
-
   postElement.classList.add('post')
+
   postElement.innerHTML = `<section class="user-info">
                 <div class="user-info-container">
                     <div class="avatar-container">
@@ -140,9 +141,7 @@ function createPostElement(data) {
                         </div>
                     </div>
                 </div>
-                <p class="likes-counter ss-normal text-normal m-0 primary-font-color">
-                Liked by <a class="ss-bold primary-font-color txt-decoration-none" href="/${username}">${username}</a> and 
-                <a class="ss-bold primary-font-color txt-decoration-none" href="/p/${id}/liked-by/">others</a></p>
+                <div class="likedby-container ss-normal text-normal m-0 primary-font-color"></div>
                 <div class="post-caption-container">
                     <p class="post-caption text-normal m-0 primary-font-color">
                         <a href="/${username}" class="username primary-font-color text-normal ss-bold txt-decoration-none pointer">${username}</a><span class="caption text-normal ss-normal">${caption}</span>
@@ -154,6 +153,10 @@ function createPostElement(data) {
   )}</time>
                 </div>
             </section>`
+
+  const likedByContainer = postElement.querySelector('.likedby-container')
+  const likedByElement = renderLikes(likedBy, id)
+  likedByContainer.appendChild(likedByElement)
 
   return postElement
 }
@@ -168,10 +171,6 @@ function renderPosts(posts) {
 }
 
 renderPosts(posts)
-
-function hasChildElement(parent, childSelector) {
-  return parent.querySelector(childSelector) !== null
-}
 
 function setupEventListeners() {
   doubleClickPostHandler()
@@ -296,5 +295,23 @@ function formatDateAsMonthDayYear(date) {
   const formattedMonth = getMonthNameFromNumber(month)
   const formattedDate = `${formattedMonth} ${formattedDay}, ${year}`
   return formattedDate
+}
+
+function renderLikes(likedByArr, postId) {
+  const likesWrapper = document.createElement('div')
+  likesWrapper.classList.add('likes-wrapper')
+
+  switch (likedByArr.length) {
+    case 0:
+      break
+    case 1:
+      likesWrapper.innerHTML = `Liked by <a class="ss-bold primary-font-color txt-decoration-none" href="/${likedByArr[0]}">${likedByArr[0]}</a>`
+      break
+    default:
+      likesWrapper.innerHTML = `Liked by <a class="ss-bold primary-font-color txt-decoration-none" href="/${likedByArr[0]}">${likedByArr[0]}</a> and 
+                <a class="ss-bold primary-font-color txt-decoration-none" href="/p/${postId}/liked-by/">others</a></div>`
+  }
+
+  return likesWrapper
 }
 
