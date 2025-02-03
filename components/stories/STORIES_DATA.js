d81ba7586395cb8b69143a4bc5f2ff377ede069a
Eduardo Henrique <itsduzao@outlook.com>
2025-02-03 19:06:51 -0300

    feat: add new images

--- components/stories/STORIES_DATA.js
+++ components/stories/STORIES_DATA.js
@@ -5,5 +5,8 @@ export const STORIES_DATA = [
   { username: 'picasso1881', url: '/img/stories/avatar-picasso1881.jpg' },
   { username: 'redon1840', url: '/img/stories/avatar-redon1840.jpg' },
   { username: 'tmaral1886', url: '/img/stories/avatar-tmaral1886.jpg' },
+  { username: 'portinari1903', url: '/img/stories/avatar-portinari1903.jpg' },
+  { username: 'kahlo1907', url: '/img/stories/avatar-kahlo1907.jpg' },
+  { username: 'rousseau1844', url: '/img/stories/avatar-rousseau1844.jpg' },
 ]
 
