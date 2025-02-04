0670eb1263481fae7b7d423db8b726e21e30ce26
Eduardo Henrique <itsduzao@outlook.com>
2025-02-04 18:04:57 -0300

    fix: update image paths in STORIES_DATA for correct referencing

--- components/stories/STORIES_DATA.js
+++ components/stories/STORIES_DATA.js
@@ -1,12 +1,18 @@
 export const STORIES_DATA = [
-  { username: 'dali1904', url: '/img/stories/avatar-dali1904.jpg' },
-  { username: 'gauguin1848', url: '/img/stories/avatar-gauguin1848.png' },
-  { username: 'monet1840', url: '/img/stories/avatar-monet1840.jpg' },
-  { username: 'picasso1881', url: '/img/stories/avatar-picasso1881.jpg' },
-  { username: 'redon1840', url: '/img/stories/avatar-redon1840.jpg' },
-  { username: 'tmaral1886', url: '/img/stories/avatar-tmaral1886.jpg' },
-  { username: 'portinari1903', url: '/img/stories/avatar-portinari1903.jpg' },
-  { username: 'kahlo1907', url: '/img/stories/avatar-kahlo1907.jpg' },
-  { username: 'rousseau1844', url: '/img/stories/avatar-rousseau1844.jpg' },
+  { username: 'dali1904', url: '../../img/stories/avatar-dali1904.jpg' },
+  { username: 'gauguin1848', url: '../../img/stories/avatar-gauguin1848.png' },
+  { username: 'monet1840', url: '../../img/stories/avatar-monet1840.jpg' },
+  { username: 'picasso1881', url: '../../img/stories/avatar-picasso1881.jpg' },
+  { username: 'redon1840', url: '../../img/stories/avatar-redon1840.jpg' },
+  { username: 'tmaral1886', url: '../../img/stories/avatar-tmaral1886.jpg' },
+  {
+    username: 'portinari1903',
+    url: '../../img/stories/avatar-portinari1903.jpg',
+  },
+  { username: 'kahlo1907', url: '../../img/stories/avatar-kahlo1907.jpg' },
+  {
+    username: 'rousseau1844',
+    url: '../../img/stories/avatar-rousseau1844.jpg',
+  },
 ]
 
